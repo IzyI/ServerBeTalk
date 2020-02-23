01d1e6971c5db1e23386d5d13496921301a30cae
mushroom <izyice@protonmail.com>
2020-02-23 18:52:14 +0300

    Update config_tg.py

--- config/config_tg.py
+++ config/config_tg.py
@@ -4,11 +4,10 @@ from dataclasses import dataclass
 @dataclass(order=True)
 class ConfTg:
     # Тестовые конфиги
-    # curl -v -x socks5://suka:vneq9t4DSW3d@205.204.87.15:1490 http://2ip.ru
-    proxy = ['205.204.87.15', '205.204.87.15']
+    proxy = ['205.204.87.43', '205.204.87.23']
     proxy_port: int = 1490
     proxy_login='suka'
     proxy_password='vneq9t4DSW3d'
-    phone: str = '79293621775'
+    phone: str = '234234535'
     id: int = 217542
     hash: str = '32d95beedc85b2a078147e9ac2d4c6ac'
